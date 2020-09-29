de5803ae2d392a4aaf2695b7546c7bce4b766ff7
Jerold Lozares <pangz.lab@gmail.com>
2020-09-29 17:38:00 +0900

    Removed the data and made it available externally

--- lib/model/navigation_model.dart
+++ lib/model/navigation_model.dart
@@ -6,11 +6,3 @@ class NavigationModel {
 
   NavigationModel({this.title, this.icon});
 }
-
-List<NavigationModel> navigationItems = [
-  NavigationModel(title: "Dashboard", icon: Icons.insert_chart),
-  NavigationModel(title: "Errors", icon: Icons.error),
-  NavigationModel(title: "Search", icon: Icons.search),
-  NavigationModel(title: "Notifications", icon: Icons.notifications),
-  NavigationModel(title: "Settings", icon: Icons.settings),
-];
